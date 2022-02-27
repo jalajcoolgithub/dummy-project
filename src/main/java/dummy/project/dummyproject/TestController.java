b8d232959b53049fe001eb30273a67003431e04e
jalajcoolgithub <jalajgupta97@gmail.com>
2022-02-27 13:43:53 +0530

    test

--- src/main/java/dummy/project/dummyproject/TestController.java
+++ src/main/java/dummy/project/dummyproject/TestController.java
@@ -6,13 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-	@GetMapping("/")
-	public String welcome() {
-		return "welcome in the microservice";
-	}
 
-	@GetMapping("/test")
-	public String test() {
-		return "tested";
-	}
 }
