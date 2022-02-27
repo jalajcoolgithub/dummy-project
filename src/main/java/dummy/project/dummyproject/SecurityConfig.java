69a20aa12506be42f4105930029419d0ef64d98f
jalajcoolgithub <jalajgupta97@gmail.com>
2022-02-27 17:02:05 +0530

    test

--- src/main/java/dummy/project/dummyproject/SecurityConfig.java
+++ src/main/java/dummy/project/dummyproject/SecurityConfig.java
@@ -1,9 +1,26 @@
 package dummy.project.dummyproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}12345").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("jalaj").password("{noop}12345").roles("USER");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.csrf().disable().authorizeRequests().antMatchers("/users/admin/**").hasAnyRole("ADMIN").and().formLogin();
+
+		http.csrf().disable().authorizeRequests().antMatchers("/users/user/**").hasAnyRole("USER").and().formLogin();
+
+	}
 }
